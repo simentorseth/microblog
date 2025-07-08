a1cb4f2b47d271d9187798a5d4e1927ee7acd7e4
simentorseth <simen.torseth@gmail.com>
2025-07-08 19:54:08 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "something else"
+    TEMPLATES_AUTO_RELOAD = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(base_dir, "app.db")
