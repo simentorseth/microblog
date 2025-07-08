c6b0faeb56f11036efab15ec3136be97a876bc60
simentorseth <simen.torseth@gmail.com>
2025-07-08 20:35:43 +0200

    Added database model for posts

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from datetime import datetime
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
@@ -9,7 +10,18 @@ class User(db.Model):
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
 
     def __repr__(self):
-        return f"<User {self.username}"
+        return f"<User {self.username}>"
     
+
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140), index=True)
+    timestamp: so.Mapped[datetime] = so.mapped_column(sa.DateTime, index=True)
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    author: so.Mapped[User] = so.relationship(back_populates="posts")
+
+    def __repr__(self):
+        return f"<Post {self.body}>"
