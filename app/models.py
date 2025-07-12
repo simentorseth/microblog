12ab5b313a43644cdfef1d13684950abbaaa8675
simentorseth <simen.torseth@gmail.com>
2025-07-12 11:35:01 +0200

    Added password hashing and verification to User model.

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from typing import Optional
 from datetime import datetime, timezone
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
 
 
 class User(db.Model):
@@ -11,6 +12,12 @@ class User(db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f"<User {self.username}>"
