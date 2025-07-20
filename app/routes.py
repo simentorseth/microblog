02c5c07b38384621bf39f5bb85ac60567b7558d1
simentorseth <simen.torseth@gmail.com>
2025-07-20 09:38:43 +0200

    Added new fields to profile template and added before request hook to update the  field

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from urllib.parse import urlsplit
@@ -79,3 +80,10 @@ def profile(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template("profile.html", user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
