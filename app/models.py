25154ad1669934f7869317b8208205e87de99c5c
simentorseth <simen.torseth@gmail.com>
2025-07-20 12:57:28 +0200

    Added followers table and following relationship to the User model

--- app/models.py
+++ app/models.py
@@ -8,6 +8,14 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 
 
+followers = sa.Table(
+    "followers",
+    db.metadata,
+    sa.Column("follower_id", sa.Integer, sa.ForeignKey("user.id"), primary_key=True),
+    sa.Column("followed_id", sa.Integer, sa.ForeignKey("user.id"), primary_key=True)
+)
+
+
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
@@ -16,6 +24,18 @@ class User(UserMixin, db.Model):
     posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
     about: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(sa.DateTime, default=lambda: datetime.now(timezone.utc))
+    following: so.WriteOnlyMapped["User"] = so.relationship(
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        back_populates="followers",
+    )
+    followers: so.WriteOnlyMapped = so.relationship(
+        secondary=followers,
+        primaryjoin=(followers.c.followed_id == id),
+        secondaryjoin=(followers.c.follower_id == id),
+        back_populates="following",
+    )
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
