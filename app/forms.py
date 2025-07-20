3886427a27eb90704a9f360e6f595f13e19faf34
simentorseth <simen.torseth@gmail.com>
2025-07-20 10:04:06 +0200

    Added profile editor

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, ValidationError, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, ValidationError, EqualTo, Length
 import sqlalchemy as sa
 from app import db
 from app.models import User
@@ -29,3 +29,9 @@ class RegistrationForm(FlaskForm):
         user = db.session.scalar(sa.select(User).where(User.email == email.data))
         if user is not None:
             raise ValidationError("Please use a different email address.")
+        
+
+class EditProfileForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    about = TextAreaField("About", validators=[Length(min=0, max=140)])
+    submit = SubmitField("Submit")
--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from urllib.parse import urlsplit
 import sqlalchemy as sa
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 
 
@@ -80,6 +80,22 @@ def profile(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template("profile.html", user=user, posts=posts)
+
+
+@app.route("/edit_profile", methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about = form.about.data
+        db.session.commit()
+        flash("Your changes have been saved.")
+        return redirect(url_for("edit_profile"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.about.data = current_user.about
+    return render_template("edit_profile.html", title="Edit profile", form=form)
 
 
 @app.before_request
